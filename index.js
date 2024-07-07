43614089df735931b5319a9b63a4a63ab71e606f
CodySt0 <Codistgeorge@gmail.com>
2024-07-07 16:21:16 -0700

    adding functions

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "0c051d7eaf8d4014a3a211953240707";
+
+weatherForm.addEventListener("submit", (event) => {});
+
+//gets weather data
+async function getWeatherData(city) {}
+
+//displays weather info
+function displayWeatherInfo(data) {}
+
+//gets emjoi for weather
+function getWeatherEmoji(weatherId) {}
+
+//message to be displayed in case of error
+function displayError(message) {}
