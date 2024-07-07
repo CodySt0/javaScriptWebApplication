cbf6e60b68eec2d0e2a061781fc66e27620ce2f8
CodySt0 <Codistgeorge@gmail.com>
2024-07-07 16:54:50 -0700

    added a new api key and functions

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = "0c051d7eaf8d4014a3a211953240707";
+const apiKey = "89e03460f09cfcdbcf5648b034d3209e";
 
-weatherForm.addEventListener("submit", (event) => {});
+weatherForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const city = cityInput.value;
+
+  if (city) {
+    try {
+      const weatherData = await getWeatherData(city);
+      displayWeatherInfo(weatherData);
+    } catch (error) {
+      console.log(error);
+      displayError(error);
+    }
+  } else {
+    displayError("Please Enter A Valid City");
+  }
+});
 
 //gets weather data
-async function getWeatherData(city) {}
+async function getWeatherData(city) {
+  const apiUrl = `http://api.weatherapi.com/v1`;
+}
 
 //displays weather info
 function displayWeatherInfo(data) {}
@@ -15,4 +33,13 @@ function displayWeatherInfo(data) {}
 function getWeatherEmoji(weatherId) {}
 
 //message to be displayed in case of error
-function displayError(message) {}
+function displayError(message) {
+  const errorDisplay = document.createElement("p");
+  errorDisplay.textContent = message;
+  //adds the css properties to errorDisplay
+  errorDisplay.classList.add("errorDisplay");
+  //resets card content
+  card.textContent = "";
+  card.computedStyleMap.display = "flex";
+  card.appendChild(errorDisplay);
+}
